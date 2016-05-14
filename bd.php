1b3902966b39c7adbf9ebcd05bb004a215cff629
fabio931217 <Lkto@MacBook-Air-de-Ember.local>
2016-05-14 00:15:44 -0500

    finis version

---
+++ bd.php
@@ -0,0 +1,223 @@
+<?php 
+
+$pregunta = array();
+$pregunta[1] =  "I have a friend ____ calls me a lot to _____ about her problems." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Who / Talk"; //Correcta
+    $respuestas[2]= "do / Talk";
+    $respuestas[3]= "Who / Talked" ;
+
+$pregunta[1] =  array('Pregunta' => $pregunta[1] ,'respuestas' => $respuestas);
+
+$pregunta[2] = "My best friend _____ a guitar. She _____ it from a rock star." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Has /Bought"; //Correcta
+    $respuestas[2]= "Had /Bought";
+    $respuestas[3]= "Has /talk" ;
+
+$pregunta[2] =  array('Pregunta' => $pregunta[2] ,'respuestas' => $respuestas);
+
+$pregunta[3] = "Ana _____ _____ to college while Steve attended a college nearby." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Went away"; //Correcta
+    $respuestas[2]= "do away";
+    $respuestas[3]= "Went doing" ;
+
+$pregunta[3] =  array('Pregunta' => $pregunta[3] ,'respuestas' => $respuestas);
+
+$pregunta[4] = "It´s more fun to ___ ___ with someone you know than go on a “blind date”." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Go out"; //Correcta
+    $respuestas[2]= "Go up";
+    $respuestas[3]= "Go back" ;
+
+$pregunta[4] =  array('Pregunta' => $pregunta[4] ,'respuestas' => $respuestas);
+
+$pregunta[5] = "My neighbors are______ unfriendly.  _____ They think they´re better than everybody else." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "A little bit / Maybe"; //Correcta
+    $respuestas[2]= "A litle bit/ Maybe";
+    $respuestas[3]= "A little bet/ Maibe" ;
+
+$pregunta[5] =  array('Pregunta' => $pregunta[5] ,'respuestas' => $respuestas);
+
+$pregunta[6] = "After you ______ ___ with someone, you should try and stay friends" ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Break up"; //Correcta
+    $respuestas[2]= "Break on";
+    $respuestas[3]= "Break back" ;
+
+$pregunta[6] =  array('Pregunta' => $pregunta[6] ,'respuestas' => $respuestas);
+
+$pregunta[7] = "Do you ____ in touch with any of the friends you ____ up With?." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Keep / grew"; //Correcta
+    $respuestas[2]= "Keeping / grew";
+    $respuestas[3]= "Keep / grow" ;
+
+$pregunta[7] =  array('Pregunta' => $pregunta[7] ,'respuestas' => $respuestas);
+
+$pregunta[8] = "Yesterday I saw a movie where a man _____ ____ of prison." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Broke out "; //Correcta
+    $respuestas[2]= "Broken out";
+    $respuestas[3]= "Broken down" ;
+
+$pregunta[8] =  array('Pregunta' => $pregunta[8] ,'respuestas' => $respuestas);
+
+
+$pregunta[9] =  "If I work ____ much, I ___ tired." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "too / get "; //Correcta
+    $respuestas[2]= "tool / get ";
+    $respuestas[3]= "too / got " ;
+
+$pregunta[9] =  array('Pregunta' => $pregunta[9] ,'respuestas' => $respuestas);
+
+$pregunta[10] = "If she ______ know the _____, she keeps ___." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "doesn't / answer / silent"; //Correcta
+    $respuestas[2]= "didn't / answer / silen";
+    $respuestas[3]= "doesn't / ansuer / cilent" ;
+
+$pregunta[10] =  array('Pregunta' => $pregunta[10] ,'respuestas' => $respuestas);
+
+$pregunta[11] = "If you ____ too much, you'll ___ drunk." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "drink / get "; //Correcta
+    $respuestas[2]= "drinking / get ";
+    $respuestas[3]= "drink / got " ;
+
+$pregunta[11] =  array('Pregunta' => $pregunta[11] ,'respuestas' => $respuestas);
+
+$pregunta[12] = "If we _____ ____ to work today, we ____ ____ a picnic." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "didn’t have / could make "; //Correcta
+    $respuestas[2]= "doesn’t havd / could had ";
+    $respuestas[3]= "don’t have / could had" ;
+
+$pregunta[12] =  array('Pregunta' => $pregunta[12] ,'respuestas' => $respuestas);
+
+$pregunta[13] = "If He ____ a superhero, He ___  fly." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Were / Would "; //Correcta
+    $respuestas[2]= "We / Would";
+    $respuestas[3]= "Were / had" ;
+
+$pregunta[13] =  array('Pregunta' => $pregunta[13] ,'respuestas' => $respuestas);
+
+$pregunta[14] = "1.	What would you ___, if you ______ something in a store?." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Do / broke"; //Correcta
+    $respuestas[2]= "Did / Break";
+    $respuestas[3]= "Do / Break" ;
+
+$pregunta[14] =  array('Pregunta' => $pregunta[14] ,'respuestas' => $respuestas);
+
+$pregunta[15] = "If I _____ the lottery, I could _____ an airplane." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Won / Buy"; //Correcta
+    $respuestas[2]= "Won / Bad";
+    $respuestas[3]= "Will / have" ;
+
+$pregunta[15] =  array('Pregunta' => $pregunta[15] ,'respuestas' => $respuestas);
+
+$pregunta[16] = "If she ____ 3 children right now, she would ______ them." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Had / Sell"; //Correcta
+    $respuestas[2]= "Haven't / Sell";
+    $respuestas[3]= "Had / Selling" ;
+
+$pregunta[16] =  array('Pregunta' => $pregunta[16] ,'respuestas' => $respuestas);
+
+$pregunta[17] = "My mother ____ the Cd´s ______ in the living room." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Put / Away"; //Correcta
+    $respuestas[2]= "do / Away";
+    $respuestas[3]= "will / Away" ;
+
+$pregunta[17] =  array('Pregunta' => $pregunta[17] ,'respuestas' => $respuestas);
+
+$pregunta[18] = "My sister is printing ____ her homework ." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Out"; //Correcta
+    $respuestas[2]= "Back";
+    $respuestas[3]= "Up" ;
+
+$pregunta[18] =  array('Pregunta' => $pregunta[18] ,'respuestas' => $respuestas);
+
+$pregunta[19] = "Camila ______ in the Computer because she needed to do a video call with her boyfriend." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Pluged"; //Correcta
+    $respuestas[2]= "Plugin";
+    $respuestas[3]= "Plugeding" ;
+
+$pregunta[19] =  array('Pregunta' => $pregunta[19] ,'respuestas' => $respuestas);
+
+$pregunta[20] = "How do you ____ on the DVD player?." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Turn"; //Correcta
+    $respuestas[2]= "Turned";
+    $respuestas[3]= "Turned off" ;
+
+$pregunta[20] =  array('Pregunta' => $pregunta[20] ,'respuestas' => $respuestas);
+
+$pregunta[21] = "If you don´t know who Cristian was, ____ him ___ online." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Look / Up"; //Correcta
+    $respuestas[2]= "Look / On";
+    $respuestas[3]= "Look / Back" ;
+
+$pregunta[21] =  array('Pregunta' => $pregunta[21] ,'respuestas' => $respuestas);
+
+$pregunta[22] = "I _____  ___the book which had fallen onto the floor." ;
+
+    $respuestas  = array();
+    $respuestas[1]= " Picked / Up "; //Correcta
+    $respuestas[2]= " Picked / On";
+    $respuestas[3]= " Picked / Down" ;
+
+$pregunta[22] =  array('Pregunta' => $pregunta[22] ,'respuestas' => $respuestas);
+
+$pregunta[23] = "Could you ____ the TV off and ___ it in this table?." ;
+
+    $respuestas  = array();
+    $respuestas[1]= "Take/ Put"; //Correcta
+    $respuestas[2]= "Take/ Down";
+    $respuestas[3]= "Take/ In" ;
+
+$pregunta[23] =  array('Pregunta' => $pregunta[23] ,'respuestas' => $respuestas);
+
+$pregunta[24] = "He´s  _______ up a computer." ;
+
+    $respuestas  = array();
+    $respuestas[1]= " Hooking "; //Correcta
+    $respuestas[2]= " Hook";
+    $respuestas[3]= " Hooking Up" ;
+
+$pregunta[24] =  array('Pregunta' => $pregunta[24] ,'respuestas' => $respuestas);
+
+echo json_encode($pregunta, JSON_PRETTY_PRINT);
+
+?>
